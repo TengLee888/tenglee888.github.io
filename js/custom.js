599551d3ffa1c8b2199e012ace014c6d65f57f7d
TengLee888 <coffeealcohol@gmail.com>
2016-07-18 21:43:47 +0800

    delete beauty1

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
 
 	$.backstretch([
-	    "https://tenglee888.github.io/mywebsite/photo/beauty1.jpg"
-	    , "https://tenglee888.github.io/mywebsite/photo/cappadociaWide.JPG"
+	    "https://tenglee888.github.io/mywebsite/photo/cappadociaWide.JPG"
 	    , "https://tenglee888.github.io/mywebsite/photo/riaSunSetWide.JPG"
 	    , "https://tenglee888.github.io/mywebsite/photo/mountainWide.JPG"
 	    ], {duration: 3000, fade: 750});
